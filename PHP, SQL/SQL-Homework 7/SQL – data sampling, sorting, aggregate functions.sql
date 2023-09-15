6efc9284d667b6b7a507341875b8ab19eae9dc38
Bbunok991 <104324674+Bbunok991@users.noreply.github.com>
2023-09-16 00:57:18 +0300

    SQL – data sampling, sorting, aggregate functions.sql

--- PHP, SQL/SQL-Homework 7/SQL – data sampling, sorting, aggregate functions.sql
+++ PHP, SQL/SQL-Homework 7/SQL – data sampling, sorting, aggregate functions.sql
@@ -1,21 +1,5 @@
 USE vk;
 
-/*Написать скрипт, отмечающий несовершеннолетних пользователей как неактивных 
-(поле is_active = true). При необходимости предварительно добавить такое поле в таблицу profiles со значением по умолчанию = false 
-(или 0) (ALTER TABLE + UPDATE)*/
-
--- ALTER TABLE vk.profiles 
--- ADD COLUMN is_active BIT DEFAULT 1;
-
--- UPDATE profiles
--- SET is_active = 0
--- WHERE birthday > (now() - INTERVAL 18 YEAR);
-
--- Написать скрипт, удаляющий сообщения «из будущего» (дата позже сегодняшней) (DELETE)
-
-/*DELETE FROM messages
-WHERE created_at > now();*/
-
 -- Написать скрипт, возвращающий список имен (только firstname) пользователей без повторений в алфавитном порядке. [ORDER BY]
 /*SELECT DISTINCT firstname
 FROM users
